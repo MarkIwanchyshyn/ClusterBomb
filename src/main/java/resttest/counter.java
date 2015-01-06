7ef3ab50723dda27f3130aa64c8e2d59440fb4da
MarkIwanchyshyn <mark.i@sympatico.ca>
2015-01-06 16:06:26 -0500

    tomcat connector working

--- src/main/java/resttest/counter.java
+++ src/main/java/resttest/counter.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import resttest.sql.TestConnector;
 
 @Controller
 @RequestMapping("/count")
@@ -13,7 +14,9 @@ public class counter {
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public String getCount(){
-        return "{ sweg: " + count++ +"}";
+        return "{ sweg: " + count++ +","+
+                "all: " + TestConnector.getAll() +
+                "}";
     }
 
 }
